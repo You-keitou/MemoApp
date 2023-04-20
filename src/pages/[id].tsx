5228c24f3a83d0cbe0ffd00df7561e1b90eec2e0
jinyang <youkeitou327@gmail.com>
2023-04-21 04:32:36 +0900

    ErrorPage、Loaderページ追加、その他もろもろ修正

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -8,20 +8,21 @@ import useAspidaSWR from '@aspida/swr'
 import { apiClient } from '~/utils/apiClient'
 
 const index: NextPage = () => {
+  //ルーターの宣言、および現在のメモのIDを宣言する
   const router = useRouter()
   const [currentMemoId, setCurrentMemoId] = useState<string>()
 
+  //ローターの変更を監視する、変更があれば現在のメモのIDを更新する
   useEffect(() => {
-    if (router.isReady) {
-      setCurrentMemoId(router.query.id as string)
-    }
-  }, [router, router.isReady])
-
+    setCurrentMemoId((router.query.id as string) || undefined)
+  }, [router])
+  //すべてのメモを取得する
   const {
     data: memos,
     error: memosError,
     mutate
   } = useAspidaSWR(apiClient.memos, {})
+
   if (memosError) return <div>failed to load</div>
   if (!memos) return <div>loading...</div>
 
